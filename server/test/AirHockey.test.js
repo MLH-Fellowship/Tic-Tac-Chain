19b3a598f0a10a80963f80b09b3f38d050e83800
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-03-09 21:59:03 +0530

    Comments added to the tests

--- server/test/AirHockey.test.js
+++ server/test/AirHockey.test.js
@@ -1,18 +1,25 @@
+// loaded the AirHockey Contract as a artifact
 const AirHockey = artifacts.require("./AirHockey.sol");
 
 contract('AirHockey',(accounts)=>{
     let airhockey;
-    
+
+    // before other code execute we need to deploy our airhockey contract
     before(async ()=>{
         airhockey=await AirHockey.deployed();
     });
 
     describe('deployment',async()=>{
+        // Test for whether the contract deployed successfully or not
         it('deploys successfully',async ()=>{
             const address=await airhockey.address;
+            // Test that check whether address is 0x0 or not
             assert.notEqual(address,0x0);
+            // Test that check whether address is a empty string or not
             assert.notEqual(address,'');
+            // Test that check whether address is a null value or not
             assert.notEqual(address,null);
+            // Test that check whether address is undefined or not
             assert.notEqual(address,undefined);
         }
     )
