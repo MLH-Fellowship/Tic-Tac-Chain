e56920049faeaaf0d22f1401a403e288b1d8cd20
imabp <abir.pal899@gmail.com>
2021-03-10 14:49:58 +0530

    Added Board Component and React App

---
+++ client/src/Game.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Board from './components/pages/Board'
+
+
+const Game = () => (
+<Board/>
+)
+ 
+export default Game
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
