461e1cb925b119efda695d7a71e6bdb4559a14c9
imabp <abir.pal899@gmail.com>
2021-03-11 00:44:27 +0530

    Add Make Room Code

---
+++ server/src/rooms/makeRoom/index.js
@@ -0,0 +1,13 @@
+import { randRoom } from "../../utilities/randRoom";
+
+// Promise function to make sure, every time a new room is generated.
+const makeRoom = (rooms, resolve) => {
+  var newRoom = randRoom();
+  while (rooms.has(newRoom)) {
+    newRoom = randRoom();
+  }
+  rooms.set(newRoom, {roomId:newRoom, players:[], board:null})
+  resolve(newRoom)
+};
+
+module.exports = {makeRoom}
