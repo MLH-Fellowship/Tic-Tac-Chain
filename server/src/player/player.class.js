9d7ca16c67991fd43f1b8ac730815a0a60e0155e
imabp <abir.pal899@gmail.com>
2021-03-11 12:37:34 +0530

    Add Player Class

---
+++ server/src/player/player.class.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(name, room, id, piece='') {
+        this.name = name
+        this.room = room
+        this.id = id
+        this.piece = piece
+    }
+}
+
+module.exports = Player
