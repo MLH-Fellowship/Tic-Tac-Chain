a96340da613dcca48a2852595993cc114080bcdb
amansharma2910 <amansharma2910@gmail.com>
2021-03-11 22:11:10 +0530

    Add landing page buttons

---
+++ client/src/components/functional/HomeButtons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingButton from './LandingButton';
+
+const HomeButtons = ({logo, onChoice}) => {
+    return (
+        <div className='choice-container'>
+            <a href="/"><img src={logo} alt='TicTacToe Game'/></a>
+            <LandingButton onChoice={onChoice} type='primary' choice='play-game' label='Play Game'/> 
+            <LandingButton onChoice={onChoice} type='secondary' choice='rules' label='Rules'/> 
+        </div>
+    );
+}
+
+export default HomeButtons;
