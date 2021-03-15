a68736a2274a625aa7c0f000a8386179d3d004c2
imabp <abir.pal899@gmail.com>
2021-03-15 11:40:46 +0530

    Remove Binding from GameChoice

--- client/src/components/functional/GameChoice.js
+++ client/src/components/functional/GameChoice.js
@@ -6,10 +6,10 @@ const GameChoice = ({gameType, heading, tagLine, onChoice}) =>{
         <div className={`choice-container-${gameType}`}>
             <h1 className="choice-head">{heading}</h1>
             <h3 className="choice-sub">{tagLine}</h3>
-            <ChoiceButton type="choice" choice={`start-game-${gameType}`} label="Start Game" onChoice={onChoice.bind(this, choice)}/>
-            <ChoiceButton type="choice" choice={`join-game-${gameType}`} label="Join Game" onChoice={onChoice.bind(this, choice)}/>
+            <ChoiceButton type="choice" choice={`start-game-${gameType}`} label="Start Game" onChoice={onChoice}/>
+            <ChoiceButton type="choice" choice={`join-game-${gameType}`} label="Join Game" onChoice={onChoice}/>
         </div>
-    )
+    )   
 }
 
 export default GameChoice;
