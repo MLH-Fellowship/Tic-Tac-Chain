51339103a1853642fc9668950d57a64c60b839fe
amansharma2910 <amansharma2910@gmail.com>
2021-03-15 11:29:16 +0530

    Add game choice screen component

--- client/src/components/pages/ChoiceScreen.js
+++ client/src/components/pages/ChoiceScreen.js
@@ -6,13 +6,26 @@ class ChoiceScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            // enter required states
         }
+    }
+
+    freeHead = "Play For Free!";
+    freeTag = "Best mode for a quick game with friends!";
+
+    betHead = "Bet 0.001 Ether!";
+    betTag = "Win cryptocurrency the easy way!";
+
+    onButtonCLick(){
+        // enter the onChoice method
     }
 
     render(){
         return (
-            <div>"sdfsdfdsfd"</div>
+            <div>
+                <GameChoice gameType="free" heading={freeHead} tagLine={freeTag} onChoice={onButtonCLick}/>
+                <GameChoice gameType="bet" heading={betHead} tagLine={betTag} onChoice={onButtonCLick}/>
+            </div>
         )
     }
 }
