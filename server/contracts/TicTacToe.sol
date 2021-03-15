e83eddecd94782b94aefa6fa825381655d5d7fc2
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-03-15 10:41:04 +0530

    Get Bet and Draw Game Fucntion added

--- server/contracts/TicTacToe.sol
+++ server/contracts/TicTacToe.sol
@@ -51,12 +51,31 @@ contract TicTacToe{
 
     // Function to take the winner index as input
     // It calculates the Winner address and send total bet to the person
-    
+
     function Winner(uint index) public{
         address payable winner_address=address(uint160(Game[RoomID][index]));
         uint totalBet=TotalBet();
         sendBetAmt(winner_address, totalBet);
 
+    }
+
+    // Function for Get Bet Amount By Address
+
+    function getBet(address player) public view returns(uint){
+        return BettAmt[player];
+    }
+
+    // Function to send money to both players if it is draw game
+    function Draw() public{
+
+        address payable player0_address=address(uint160(Game[RoomID][0]));
+        uint bet0=getBet(Game[RoomID][0]);
+        address payable player1_address=address(uint160(Game[RoomID][1]));
+        uint bet1=getBet(Game[RoomID][1]);
+        sendBetAmt(player0_address, bet0);
+        sendBetAmt(player1_address, bet1);
+
+        
     }
 
 }
