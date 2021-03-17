0ed6a67f443c1b0d01fd67a74b68c9d516c3c416
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-03-17 11:30:38 +0530

    player 1 creation and setting and getting unique id added

--- server/test/TicTacToe.test.js
+++ server/test/TicTacToe.test.js
@@ -27,6 +27,31 @@ contract('TicTacToe', ([accounts,player1,player2]) => {
         }
     );
 
+        // Test for setting the unique id to get the unique id mapping from Room ID
+        const roomid="qwawerdsss";
+        it('Setting the unique id from Room ID',async()=>{
+            const result=await tictactoe.setid(roomid);
+        });
+
+        var uniqueid = 0;
+
+        // Test for getting the unique id saved from Room ID setted in earlier test
+        it('Getting the unique id from Room ID',async()=>{
+            uniqueid=await tictactoe.getid(roomid);
+            // Checking whether uniqueid is equal to 1 or not
+            asserted.equal(1,uniqueid);
+            // Checking whether unique id is not equal other than 1 
+            assert.notEqual(2,uniqueid);
+        });
+
+        // Creating a Player1 and Sending the Bet amount to store in the smart contract
+
+        it('Creation of Player1 and Sending the Bet amount Successfully',async()=>{
+            const result=await tictactoe.createPlayer.sendTransaction(uniqueid,{ from: player1, 
+                value: web3.utils.toWei("0.01")});
+        })
+
+
 
 })
 })
