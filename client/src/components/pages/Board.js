660e8894befe8aa426dcb75e76b6c6cd7c28a1ee
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-03-19 23:28:27 +0530

    Rules and Wei Change in Web3

--- client/src/components/pages/Board.js
+++ client/src/components/pages/Board.js
@@ -115,7 +115,7 @@ class Board extends Component {
   async createplayer2(id) {
     const web3=window.web3;
     const account = (await web3.eth.getAccounts())[0];
-    return await this.state.ticTacToe.methods.createPlayer(id).send({from: account, value: Web3.toWei("0.01", "ether")});
+    return await this.state.ticTacToe.methods.createPlayer(id).send({from: account, value:1000000000000000});
   }
 
   async sendBettoWinner(id, index) {
